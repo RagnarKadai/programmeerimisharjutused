14a466fa31d8bf60d2e8c96c750e2ce75b3d876c
ragnarkadai <62658258+RagnarKadai@users.noreply.github.com>
2021-02-19 16:51:44 +0200

    Update Algarvuring.java

--- Algarvuring.java
+++ Algarvuring.java
@@ -1,69 +1,41 @@
-import java.util.Scanner;
-class AlgarvuRing{
-
-
+class AlgarvuRing {
     public static void main(String[] args) {
-        Scanner loeb = new Scanner(System.in);
-        System.out.print("Antud lähtekoht: ");
-        int a = loeb.nextInt();
-        algarvuRing(a);
+        algarvuRing(1000);
         System.out.println("Ragnar Kadai " + new java.sql.Timestamp(System.currentTimeMillis()));
     }
-
-
-
-        static boolean shuffle ( int arv){
-            int x = String.valueOf(arv).length();
-            int uusarv = arv;
-            boolean edasi = true;
-            for (int i = 0; i < x; i++) {
-                if (check(uusarv) && arv >= uusarv) {
-                    int viimane = uusarv % 10;
-                    int esimene = uusarv / 10;
-                    int oige = esimene + viimane * (int) Math.pow(10, (int) Math.log10(uusarv));
-                    uusarv = oige;
-                    check(oige);
-                } else {
-                    edasi = false;
-                    return false;
-                }
-            }
-            if (edasi) {
-                System.out.println(uusarv);
-                return true;
-            }
-            return false;
-        }
-
-
-
-
-        static boolean check ( int arv){
-            boolean mittealgarv = false;
-            for (int i = 2; i <= arv / 2; ++i) {
-                if (arv % i == 0) {
-                    mittealgarv = true;
-                    break;
-                }
-            }
-            if (!mittealgarv) {
-                return true;
+    static boolean shuffle(int arv) {
+        int x = String.valueOf(arv).length();
+        int uusarv = arv;
+        for (int i = 0; i < x; i++) {
+            if (check(uusarv) && arv >= uusarv) {
+                int viimane = uusarv % 10;
+                int esimene = uusarv / 10;
+                int oige = esimene + viimane * (int) Math.pow(10, (int) Math.log10(uusarv));
+                uusarv = oige;
+                check(oige);
             } else {
                 return false;
             }
         }
-
-
-
-
-        public static void algarvuRing(int a){
-            int count = 0;
-            for (int i = 1; i < a - 9 && count != 5; i++) {
-                if (check(a - i)) {
-                    if (shuffle(a - i)) {
-                        count += 1;
-                    }
+        System.out.println(uusarv);
+        return true;
+    }
+    static boolean check(int arv) {
+        for (int i = 2; i <= Math.sqrt(arv)+1; i++) {
+            if (arv % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void algarvuRing(int a){
+        int count = 0;
+        for (int i = 1; i < a - 9 && count != 5; i++) {
+            if (check(a - i)) {
+                if (shuffle(a - i)) {
+                    count += 1;
                 }
             }
         }
     }
+}
