bd34e603e047ab15c437f439ad5053022fd99bb8
ragnarkadai <62658258+RagnarKadai@users.noreply.github.com>
2021-02-19 18:02:31 +0200

    Update Algarvuring.java

--- Algarvuring.java
+++ Algarvuring.java
@@ -1,15 +1,21 @@
-// Programmeerimisharjutused. LTAT.03.007
-// 2020/2021 kevadsemester
-// Kodutöö. Ülesanne nr 1a
-// Teema: Tsüklid
-// Autor: Ragnar Kadai
-// Inspiratsioon: Praktikum 1 ül 2
-
+/*****************************************************************************
+ * Programmeerimisharjutused. LTAT.03.007
+ * 2020/2021 kevadsemester
+ *
+ * Kodutöö. Ülesanne nr 1a
+ * Teema: Tsüklid
+ *
+ * Autor: Ragnar Kadai
+ *
+ * Mõningane eeskuju:
+ * Arvude ümberpaigutuseks: Praktikum 1 ül 2
+ * Algarvu kontrollimiseks: https://www.programiz.com/java-programming/examples/prime-number
+ *****************************************************************************/
 class AlgarvuRing {
 
     public static void main(String[] args) {
         //Argument käsurealt, mis töödeldakse täisarvuks ning rakendatakse meetodit algarvuRing(kasurida)
-        int kasurida = Integer.parseInt(args[0]);
+        int kasurida = 20;
         System.out.println("Antud lähtekoht: "+ kasurida);
         System.out.println("Leitud algarvuringid:");
         algarvuRing(kasurida);
@@ -17,37 +23,30 @@ class AlgarvuRing {
     }
 
     public static void algarvuRing(int a){
-        // Tingimusel, et arv on suurem kui 9 ja count ei ole 5 kontrollib, kas arv on algarv.
-        // Kui arv on check läbinud (on algarv), siis läheb ta meetodi shuffle juurde, kus ta viimane arv läheb
-        // esimeseks ja läbib uuesti kontrolli(check). Kui arv on iga kontrolli läbinud (igal ümberpaigutamisel
-        // oli algarv), siis see algarvuringi arv prinditakse ning count tõuseb 1 võrra.
-        // Kui arv ei läbi kuskil punktis testi, siis see arv unustatakse ja minnakse järgmise x-1 arvuga edasi.
-        int count = 0; // Algarvuringide lugemiseks, et neid oleks 5
+        int count = 0; // Loeb algarvuringe
         for (int i = 1; i < a - 9 && count != 5; i++) {
-            if (check(a - i)) { // Esialgne kontroll, kas baasarv on algarv, et edasi minna
+            if (check(a - i)) { // Esialgne kontroll, et arv on algarv
                 if (shuffle(a - i)) { // Arvu ümberpaigutamine ja edasine kontroll
-                    count += 1; // Juhul, kui arv on läbinud kõik tingimused, siis shuffle meetodis prinditakse ta
-                }               // ja count suureneb 1 võrra.
+                    count += 1; // Kui arv on läbinud kõik tingimused, siis ta prinditakse ning loend suureneb
+                }
             }
         }
     }
 
     static boolean shuffle(int arv) {
         int x = String.valueOf(arv).length(); // teadmiseks mitu korda peab arvu ümber tõstma
-        int uusarv = arv; //
-        for (int i = 0; i < x; i++) {
+        int uusarv = arv; // paindlik int väärtus edasiseks kontrolliks
+        for (int i = 0; i != x; i++) {
             if (check(uusarv) && arv >= uusarv) { // Tingimus et eelnev arv oli algarv ja et ta ei olnud suurem
                 int viimane = uusarv % 10;        // kui baasarv
                 int esimene = uusarv / 10;
-                int oige = esimene + viimane * (int) Math.pow(10, (int) Math.log10(uusarv));
-                uusarv = oige;
-                check(oige);
+                uusarv = esimene + viimane * (int) Math.pow(10, (int) Math.log10(uusarv));
             }
             else {
                 return false;
             }
         }
-        System.out.println(uusarv);
+        System.out.println(arv); // Väljastab arvu, kui ta on kõik algarvu ümberpaigutamised läbinud
         return true;
     }
 
